80d323745a5125c163070b2a4030f93c1dcfd879
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2024-08-12 04:31:47 +0700

    rework test improve mini_data

--- tests/work.rs
+++ tests/work.rs
@@ -3,6 +3,7 @@ mod tests {
     use std::vec;
 
     use shuller::prelude::*; // Импортируйте нужные модули
+
     #[tokio::test]
     async fn test_of_work() {
         let instance: Posts = R34Params::init()
@@ -82,10 +83,20 @@ mod tests {
     #[cfg(feature = "rand")]
     #[tokio::test]
     async fn test_macro_random_download() {
-        let instance = R34!(R; D).unwrap();
-        let url = instance.get_f_url().unwrap();
-        let instance1 = R34!(R; D).unwrap();
-        let url1 = instance1.get_f_url().unwrap();
-        assert_ne!(url, url1);
+        let first: tokio::task::JoinHandle<Result<String, ()>> = tokio::spawn(async {
+            let instance = R34!(R; D).unwrap();
+            Ok(instance.get_f_url().unwrap().to_string())
+        });
+
+        let second: tokio::task::JoinHandle<Result<String, ()>> = tokio::spawn(async {
+            let instance = R34!(R; D).unwrap();
+            Ok(instance.get_f_url().unwrap().to_string())
+        });
+
+        let first_url = tokio::join!(first).0.unwrap().unwrap();
+        tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+        let second_url = tokio::join!(second).0.unwrap().unwrap();
+
+        assert_ne!(first_url, second_url);
     }
 }
